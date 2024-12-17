9478ca33f969bf56e8dfd0297667106856c5c0f1
Sdg877 <sdrakegill@gmail.com>
2024-12-17 08:37:34 +0000

    Adding placeholders for labels

--- weather_app.py
+++ weather_app.py
@@ -7,6 +7,10 @@ class WeatherApp(QWidget):
     def __init__(self):
         super().__init__()
         self.city_label = QLabel("Enter city name: ", self)
+        self.city_input = QLineEdit(self)
+        self.get_weather_button = QPushButton("Get Weather", self)
+        self.temperature_label = QLabel("8°C", self)
+        self.emoji_label = QLabel("")
         
 if __name__ == "__main__":
     app = QApplication(sys.argv)
