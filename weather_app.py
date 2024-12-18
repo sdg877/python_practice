ffad5b0fce00389e1beef12c32409a5b6889fd3c
Sdg877 <sdrakegill@gmail.com>
2024-12-18 09:17:05 +0000

    Widgets added

--- weather_app.py
+++ weather_app.py
@@ -11,7 +11,22 @@ class WeatherApp(QWidget):
         self.get_weather_button = QPushButton("Get Weather", self)
         self.temperature_label = QLabel("8°C", self)
         self.emoji_label = QLabel("☀️", self)
+        self.description_label = QLabel("Sunny", self)
+        self.initUI()
         
+    def initUI(self):
+        self.setWindowTitle("Weather App")
+        
+        vbox = QVBoxLayout()
+        
+        vbox.addWidget(self.city_label)
+        vbox.addWidget(self.city_input)
+        vbox.addWidget(self.get_weather_button)
+        vbox.addWidget(self.temperature_label)
+        vbox.addWidget(self.emoji_label)
+        vbox.addWidget(self.description_label)
+
+    
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     weather_app = WeatherApp()
