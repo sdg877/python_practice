80badb517d115c65ce2ecbe097b4ced4b38d3b78
Sdg877 <sdrakegill@gmail.com>
2025-01-06 07:49:19 +0000

    Stylesheet added to display error function

--- weather_app.py
+++ weather_app.py
@@ -116,6 +116,7 @@ class WeatherApp(QWidget):
             self.display_error(f"Request Error:\n{e}")
         
     def display_error(self, message):
+        self.temperature_label.setStyleSheet("font-size: 30px")
         self.temperature_label.setText(message)
         
     def display_weather(self, data):
