1c75c796accd400f2c0c9b595fd4db4c5acbceb3
Sdg877 <sdrakegill@gmail.com>
2025-01-09 07:12:09 +0000

    Project completed

--- pig.py
+++ pig.py
@@ -4,16 +4,51 @@ def roll():
     min_value = 1
     max_value = 6
     roll = random.randint(min_value, max_value)
-    
     return roll
 
+
 while True:
-    players = input("Enter the number of players (1-4): ")
+    players = input("Enter the number of players (2-4): ")
     if players.isdigit():
         players = int(players)
-        if 1 <= players <= 4:
-            print("Must be between 1-4 players.")
+        if 2 <= players <= 4:
             break
         else:
-            print("Invalid choice, try again.")
+            print("Must be between 2-4 players.") 
+    else:
+        print("Invalid choice, try again.")
+
+max_score = 50
+player_scores = [0 for _ in range(players)]
+
+
+while max(player_scores) < max_score:
+    for player_idx in range(players):
+        print(f"\nPlayer {player_idx + 1}'s turn")
+        print("Your total score is:", player_scores[player_idx], "\n")
+        current_score = 0
+
+        while True:
+            should_roll = input("Would you like to roll (y)? ")
+            if should_roll.lower() != "y":
+                break
+            
+            value = roll()
+            if value == 1:
+                print("You rolled a 1, turn over!")
+                current_score = 0
+                break
+            else:
+                current_score += value
+                print(f"You rolled a {value}.")
+                print(f"Your score this turn is: {current_score}")
         
+        
+        player_scores[player_idx] += current_score
+        print(f"Player {player_idx + 1}'s total score is now: {player_scores[player_idx]}")
+
+max_score = max(player_scores)
+winning_idx = player_scores.index(max_score)
+print("Player number", winning_idx + 1, "is the winner with a score of:",max_score)
+
+
