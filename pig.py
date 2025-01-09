1490941731c86120bd25934bbbb9a274efe45aee
Sdg877 <sdrakegill@gmail.com>
2025-01-09 06:31:28 +0000

    Project set up

---
+++ pig.py
@@ -0,0 +1,19 @@
+import random
+
+def roll():
+    min_value = 1
+    max_value = 6
+    roll = random.randint(min_value, max_value)
+    
+    return roll
+
+while True:
+    players = input("Enter the number of players (1-4): ")
+    if players.isdigit():
+        players = int(players)
+        if 1 <= players <= 4:
+            print("Must be between 1-4 players.")
+            break
+        else:
+            print("Invalid choice, try again.")
+        
