2dd0c6a746a8bca5c7e7752ea8dc9768dbe2407e
Sdg877 <sdrakegill@gmail.com>
2025-01-10 07:19:41 +0000

    Project set up

---
+++ turtle_racing.py
@@ -0,0 +1,10 @@
+def get_number_of_racers():
+    racers = 0
+    while True:
+        racers = input("Enter the number of racers (2 - 10): ")
+        if racers.isdigit():
+            racers = int(racers)
+        else:
+            print("Input is not numeric, try again..")
+            continue
+            
