b7ecdef490f5bfc299c5f6fdab2dd7fd8db10c9e
Sdg877 <sdrakegill@gmail.com>
2025-01-13 08:46:55 +0000

    Project completed

--- file_backup.py
+++ file_backup.py
@@ -9,3 +9,11 @@ destination_dir = "/Users/sylviadrake-gill/Desktop/Projects/backup"
 def copy_folder_to_directory(source, dest):
     today = datetime.date.today()
     dest_dir = os.path.join(dest, str(today))
+    
+    try:
+        shutil.copytree(source, dest_dir)
+        print(f"Folder copied to: {dest_dir}")
+    except FileExistsError:
+        print(f"Folder already exists in: {dest_dir}")
+        
+copy_folder_to_directory(source_dir, destination_dir)
