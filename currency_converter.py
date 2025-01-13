0b680fbfe7e96a3d3eb4e4be22a69af986df3a0b
Sdg877 <sdrakegill@gmail.com>
2025-01-13 08:30:08 +0000

    Project set up

--- currency_converter.py
+++ currency_converter.py
@@ -1,6 +1,30 @@
 from dotenv import load_dotenv
 import os
+from requests import get
+from pprint import PrettyPrinter
 
 load_dotenv()
 
 api_key = os.getenv("API_KEY")
+base_url = os.getenv("BASE_URL")
+
+printer = PrettyPrinter()
+
+def get_currencies():
+   
+    endpoint = "/api/v/currencies"
+    url = f"{base_url}{endpoint}?apiKey={api_key}"
+    print(f"Request URL: {url}")  
+
+    try:
+        response = get(url)
+        print(f"Response Status Code: {response.status_code}")
+        print(f"Response Content: {response.text}")
+        response.raise_for_status() 
+
+        data = response.json()
+        printer.pprint(data)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+get_currencies()
