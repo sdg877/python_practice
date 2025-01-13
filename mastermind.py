0c6e21320bd9af1e090d6457703e7d25fd918cc6
Sdg877 <sdrakegill@gmail.com>
2025-01-13 10:01:23 +0000

    Project set up

---
+++ mastermind.py
@@ -0,0 +1,21 @@
+import random
+
+COLORS = ["R", "G", "B", "Y", "W", "O"]
+TRIES = 10
+CODE_LENGTH = 4
+
+def generate_code():
+    code = []
+    
+    for _ in range(CODE_LENGTH):
+        color = random.choicr(COLORS)
+        code.append(color)
+        
+    return code
+
+def guess_code():
+    guess = input("Guess: ").upper().split(" ")
+    
+    if len(guess) != CODE_LENGTH:
+        print(f"You must guess {CODE_LENGTH} colors.")
+        
